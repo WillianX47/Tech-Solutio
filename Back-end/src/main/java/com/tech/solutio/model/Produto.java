2da082dd2e2cc58b589d7a200ff02c5be9e7661d
WillianX47 <willianx47@gmail.com>
2022-01-19 19:15:06 -0300

    [Back-end - Task: Ligação entre tabelas | 19.01.22]

--- Back-end/src/main/java/com/tech/solutio/model/Produto.java
+++ Back-end/src/main/java/com/tech/solutio/model/Produto.java
@@ -4,8 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
@@ -19,8 +22,11 @@ public class Produto {
 	private @NotNull String fornecedor;
 	
 	private @NotNull String valor;
-
 	
+	@ManyToOne
+	@JoinColumn(name = "tb_usuario")
+	@JsonIgnoreProperties({"meusProdutos"})
+	private @NotNull Usuario usuario;
 	
 	public Long getId() {
 		return id;
--- Back-end/src/main/java/com/tech/solutio/model/Usuario.java
+++ Back-end/src/main/java/com/tech/solutio/model/Usuario.java
@@ -1,13 +1,20 @@
 package com.tech.solutio.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -22,6 +29,10 @@ public class Usuario {
 	@NotBlank(message = "O atributo usuario é obrigatório!")
 	@Email(message = "O atributo usuario deve ser um email valido!")
 	private @Size(min = 5, max = 100) String usuario;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties({"usuario"})
+	private List<Produto> meusProdutos = new ArrayList<>();
 
 	public Long getId() {
 		return id;
