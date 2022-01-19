5adbe13022ef3514a4cf27c5ddcab35999375b94
WillianX47 <willianx47@gmail.com>
2022-01-19 17:27:31 -0300

    [Back-end - Task: Inicializando projeto | 19.01.22]

---
+++ Back-end/src/test/java/com/tech/solutio/SolutioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.solutio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
